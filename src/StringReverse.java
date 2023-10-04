12a173ba74f4aca49abdf59b25fc1e01352d570d
Pixi13139 <istvan.raffai@flowacademy.hu>
2023-10-04 17:58:01 +0200

    StringReverse.2

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -5,6 +5,7 @@ public class StringReverse {
         for (int i = ch.length - 1; i > -1; i--) {
             sb.append(ch[i]);
         }
+        System.out.println(sb.toString());
         return sb.toString();
     }
 }
