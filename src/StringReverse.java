5051ed97c1e28301cb553343621c3ec204e972ac
Pixi13139 <istvan.raffai@flowacademy.hu>
2023-10-04 16:51:18 +0200

    StringReverse

---
+++ src/StringReverse.java
@@ -0,0 +1,10 @@
+public class StringReverse {
+    public static String solution(String str) {
+        char[] ch = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = ch.length - 1; i > -1; i--) {
+            sb.append(ch[i]);
+        }
+        return sb.toString();
+    }
+}
